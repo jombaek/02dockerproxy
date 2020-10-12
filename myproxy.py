747474b7052a9e8afd683dfeeac2fd06db1f4294
plenzke <3njoyth3sil3nc399@gmail.com>
2020-10-12 13:32:21 +0900

    added dockerproxy

---
+++ myproxy.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import requests
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    if 'url' in request.args:
+        r = requests.get(request.args['url'])
+        bts = r.content
+        return bts
+    else :
+        return '''
+        	<form method = 'get'>
+            <input name = "url" type = "text">
+            <input type = "submit">
+            </form>
+        '''
+
+if __name__ == '__main__':
+    app.run()

1fce305879d8334f0976f772494e1ee3fa79a9b0
EIS <evierstov@gmail.com>
2020-10-26 02:19:01 +0900

    added proxy

---
+++ myproxy.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import requests
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    if 'url' in request.args:
+        r = requests.get(request.args['url'])
+        bts = r.content
+        return bts
+    else :
+        return '''
+        
+            <form method = 'get'>
+            <input name = "url" type = "text">
+            <input type = "submit">
+            </form>
+            
+        '''
+
+if __name__ == '__main__':
+    app.run()
